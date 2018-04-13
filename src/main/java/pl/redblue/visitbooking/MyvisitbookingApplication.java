7ddd3cb9b616d18a3bffe7f4f07e37a881cc2723
redbluex <redbluedev@gmail.com>
2018-04-13 13:20:16 +0200

    first commit

---
+++ src/main/java/pl/redblue/visitbooking/MyvisitbookingApplication.java
@@ -0,0 +1,12 @@
+package pl.redblue.visitbooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyvisitbookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyvisitbookingApplication.class, args);
+	}
+}
